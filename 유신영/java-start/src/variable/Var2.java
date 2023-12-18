dba7256bfb18543ae751108ac78b268e2af0dc87
cmblir <sodlalwl13@gmail.com>
2023-12-18 20:48:31 +0900

    add .java : inflean java variable chapter1

---
+++ 유신영/java-start/src/variable/Var2.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
