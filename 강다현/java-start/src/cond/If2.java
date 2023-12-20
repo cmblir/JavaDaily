f222cc18481db3315db55f118dbb6fe280fb3a84
dhkang <alceste951005@naver.com>
2023-12-20 20:33:57 +0900

    add .java : inflearn java Operator

---
+++ 강다현/java-start/src/cond/If2.java
@@ -0,0 +1,14 @@
+package cond;
+
+public class If2 {
+    public static void main(String[] args) {
+        int age = 20; //사용자 나이
+        if(age>=18){
+            System.out.println("성인입니다.");
+        }else{
+            System.out.println("미성년자 입니다.");
+        }
+
+
+    }
+}
