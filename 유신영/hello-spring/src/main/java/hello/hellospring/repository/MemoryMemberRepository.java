1caeafd33671cf5a602eccac4c3489ca119109ac
cmblir <sodlalwl13@gmail.com>
2024-01-03 13:41:32 +0900

    add .java : inflean spring member domain, repository, service chapter3

---
+++ 유신영/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null을 반환할 가능성을 고려
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
