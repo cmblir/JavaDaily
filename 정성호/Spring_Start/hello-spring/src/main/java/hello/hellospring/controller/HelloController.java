1c0517965b7dab31f04d95420a8ccc63c560f524
Your Name <kjeng7897@gmail.com>
2024-01-04 19:04:30 +0900

    add : memberserviceTest

--- 정성호/Spring_Start/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ 정성호/Spring_Start/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.*;
 
 @Controller
-public class HelloControlller {
+public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
@@ -17,5 +20,31 @@ public class HelloControlller {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http 에서 body 영역에 직접 넣어주겠다
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
