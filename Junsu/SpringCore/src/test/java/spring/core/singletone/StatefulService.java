805fea09b029bc31b090a8f370d733ec060795c3
wnstn819 <wnstn819@naver.com>
2024-01-04 21:34:37 +0900

    Section 4.4 ~ 5.4

---
+++ Junsu/SpringCore/src/test/java/spring/core/singletone/StatefulService.java
@@ -0,0 +1,15 @@
+package spring.core.singletone;
+
+public class StatefulService {
+
+    private int price;
+
+    public int order(String name, int price){
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+}
---
+++ Junsu/SpringCore/src/test/java/spring/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package spring.core.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import spring.core.member.service.MemberService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
