cfe76c7edc68f7bc0d40b18db5d349040ef08d10
crispindeity <shoy1415@gmail.com>
2024-01-07 22:46:48 +0900

    :tada: Project init

---
+++ 신희철/spring-transaction/src/main/java/com/crispin/springtransaction/SpringTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.crispin.springtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionApplication.class, args);
+    }
+
+}
---
+++ 신희철/spring-transaction/src/test/java/com/crispin/springtransaction/SpringTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crispin.springtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
