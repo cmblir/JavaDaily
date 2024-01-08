493944f35c303741c9d405179ec8d27eaa3f6056
cmblir <sodlalwl13@gmail.com>
2024-01-08 23:34:39 +0900

    temp : inflean java basic

---
+++ 유신영/java-basic/src/poly/ex2/AnimalPolyMain3.java
@@ -0,0 +1,16 @@
+package poly.ex2;
+
+public class AnimalPolyMain3 {
+    public static void main(String[] args) {
+        Animal[] animalArr = {new Dog(), new Caw(), new Caw()};
+        for (Animal animal : animalArr) {
+            soundAnimal(animal);
+        }
+    }
+
+    private static void soundAnimal(Animal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
