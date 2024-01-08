9d86143e7bd76178898c532774f42e605f01ec30
crispindeity <shoy1415@gmail.com>
2024-01-08 23:31:38 +0900

    :white_check_mark: Add: requires new transaction propagation
     ## Description
      - requires new 트랜잭션 전파 테스트 추가

--- 신희철/spring-transaction/src/test/java/com/crispin/springtransaction/propagation/BasicTransactionTest.java
+++ 신희철/spring-transaction/src/test/java/com/crispin/springtransaction/propagation/BasicTransactionTest.java
@@ -9,8 +9,10 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import javax.sql.DataSource;
@@ -122,5 +124,24 @@ class BasicTransactionTest {
         Assertions.assertThatThrownBy(
                 () -> transactionManager.commit(outer)
         ).isInstanceOf(UnexpectedRollbackException.class);
+    }
+
+    @Test
+    void innerRollBackRequiresNew() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = transactionManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = transactionManager.getTransaction(definition);
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 롤백");
+        transactionManager.rollback(inner);
+        log.info("외부 트랜잭션 커밋");
+        transactionManager.commit(outer);
+
     }
 }
