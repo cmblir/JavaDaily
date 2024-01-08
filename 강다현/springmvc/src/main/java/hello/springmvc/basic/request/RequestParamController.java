d497fb77091277de3a03471f000592de121f41f6
dhkang <alceste951005@naver.com>
2024-01-08 23:52:54 +0900

    add .java : infrean spring MVC1 chapter 7

---
+++ 강다현/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,35 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username = {}, age = {}", username, age);
+
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody //String을 반환하지만, 뷰를 찾지 않도록 하고 싶을떼,, RestController와 같게 동작한다.
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge
+    ) {
+        log.info("username = {}, age = {}", memberName, memberAge);
+        return "ok";
+    }
+}
