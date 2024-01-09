ec5c562b3632eb34330404fefc63544db9dde04f
crispindeity <shoy1415@gmail.com>
2024-01-09 20:53:20 +0900

    :sparkles: Feat: transaction example
     ## Description
      - 트랜잭션 전파 마지막 예제

--- 신희철/spring-transaction/src/test/java/com/crispin/springtransaction/propagation/MemberServiceTest.java
+++ 신희철/spring-transaction/src/test/java/com/crispin/springtransaction/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 @Slf4j
 @SpringBootTest
@@ -48,6 +49,98 @@ class MemberServiceTest {
         // when
         Assertions.assertThatThrownBy(() ->memberService.joinV1(username))
                 .isInstanceOf(RuntimeException.class);
+
+        // then
+        Assertions.assertThat(memberRepository.find(username)).isPresent();
+        Assertions.assertThat(logRepository.find(username)).isEmpty();
+    }
+
+    /**
+     * memberService    @Transaction: ON
+     * memberRepository @Transaction: OFF
+     * logRepository    @Transaction: OFF
+     */
+    @Test
+    void singleTransaction() {
+        // given
+        String username = "singleTransaction";
+
+        // when
+        memberService.joinV1(username);
+
+        // then
+        Assertions.assertThat(memberRepository.find(username)).isPresent();
+        Assertions.assertThat(logRepository.find(username)).isPresent();
+    }
+
+    /**
+     * memberService    @Transaction: ON
+     * memberRepository @Transaction: ON
+     * logRepository    @Transaction: ON
+     */
+    @Test
+    void outerTransactionOnSuccess() {
+        // given
+        String username = "outerTransactionOnSuccess";
+
+        // when
+        memberService.joinV1(username);
+
+        // then
+        Assertions.assertThat(memberRepository.find(username)).isPresent();
+        Assertions.assertThat(logRepository.find(username)).isPresent();
+    }
+
+    /**
+     * memberService    @Transaction: ON
+     * memberRepository @Transaction: ON
+     * logRepository    @Transaction: ON Exception
+     */
+    @Test
+    void outerTransactionOffFail2() {
+        // given
+        String username = "로그예외_outerTransactionOffFail2";
+
+        // when
+        Assertions.assertThatThrownBy(() ->memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        // then
+        Assertions.assertThat(memberRepository.find(username)).isEmpty();
+        Assertions.assertThat(logRepository.find(username)).isEmpty();
+    }
+
+    /**
+     * memberService    @Transaction: ON
+     * memberRepository @Transaction: ON
+     * logRepository    @Transaction: ON Exception
+     */
+    @Test
+    void recoverExceptionFail() {
+        // given
+        String username = "로그예외_recoverExceptionFail";
+
+        // when
+        Assertions.assertThatThrownBy(() ->memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
+
+        // then
+        Assertions.assertThat(memberRepository.find(username)).isEmpty();
+        Assertions.assertThat(logRepository.find(username)).isEmpty();
+    }
+
+    /**
+     * memberService    @Transaction: ON
+     * memberRepository @Transaction: ON
+     * logRepository    @Transaction: ON(REQUIRES_NEW) Exception
+     */
+    @Test
+    void recoverExceptionSuccess() {
+        // given
+        String username = "로그예외_recoverExceptionSuccess";
+
+        // when
+        memberService.joinV2(username);
 
         // then
         Assertions.assertThat(memberRepository.find(username)).isPresent();
