954ef5483497462bd83f08a48a76baff0cce574d
crispindeity <shoy1415@gmail.com>
2024-01-10 12:34:53 +0900

    :sparkles: Feat: trace v1
      - trace v1 생성

---
+++ 신희철/spring-core-advanced/src/main/java/com/crispin/springcoreadvanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,40 @@
+package com.crispin.springcoreadvanced.app.v1;
+
+import com.crispin.springcoreadvanced.trace.TraceStatus;
+import com.crispin.springcoreadvanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderRepository.request()");
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalArgumentException("예외 발생!");
+            }
+            sleep(1000);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 다시 꼭 던져주어야 한다.
+        }
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ 신희철/spring-core-advanced/src/main/java/com/crispin/springcoreadvanced/app/v1/OrderServiceV1.java
@@ -0,0 +1,27 @@
+package com.crispin.springcoreadvanced.app.v1;
+
+import com.crispin.springcoreadvanced.trace.TraceStatus;
+import com.crispin.springcoreadvanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderService.request()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 다시 꼭 던져주어야 한다.
+        }
+    }
+}
