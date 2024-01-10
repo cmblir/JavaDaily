af5f4ef5017756c99342dc77d1f30e7ce34b6f80
crispindeity <shoy1415@gmail.com>
2024-01-10 21:33:24 +0900

    :white_check_mark: Add: trace v2 test
     ## Description
      - trace v2 test 추가

---
+++ 신희철/spring-core-advanced/src/test/java/com/crispin/springcoreadvanced/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,26 @@
+package com.crispin.springcoreadvanced.trace.hellotrace;
+
+import com.crispin.springcoreadvanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV2Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("Hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalArgumentException());
+        trace.exception(status1, new IllegalArgumentException());
+    }
+
+}
