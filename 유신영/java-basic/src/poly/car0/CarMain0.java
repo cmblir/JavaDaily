2f387f50a0050303db6e2a4f556f2cff304fb2cf
cmblir <sodlalwl13@gmail.com>
2024-01-10 20:44:12 +0900

    add .java : inflean java polymorphism chapter12

---
+++ 유신영/java-basic/src/poly/car0/CarMain0.java
@@ -0,0 +1,16 @@
+package poly.car0;
+
+public class CarMain0 {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        K3Car k3Car = new K3Car();
+        driver.setK3Car(k3Car);
+        // 객체 의존 관계 의존 관계 주입
+        driver.drive();
+
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null);
+        driver.setModel3Car(model3Car);
+        driver.drive();
+    }
+}
---
+++ 유신영/java-basic/src/poly/ex/pay0/Payment.java
@@ -0,0 +1,7 @@
+package poly.ex.pay0;
+
+public abstract class Payment {
+    boolean pay(int amount) {
+        return false;
+    }
+}
