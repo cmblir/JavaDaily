6f1509e38803880faf2319c274fe5ac6aa758c6b
cmblir <sodlalwl13@gmail.com>
2024-01-10 18:33:07 +0900

    add .java : inflean java polymorphism chapter11

---
+++ 유신영/java-basic/src/poly/ex5/Cat.java
@@ -0,0 +1,13 @@
+package poly.ex5;
+
+public class Cat implements InterfaceAnimmal {
+    @Override
+    public void sound(){
+        System.out.println("야옹");
+    }
+
+    @Override
+    public void move(){
+        System.out.println("고양이 이동");
+    }
+}
