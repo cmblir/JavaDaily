6b5a548bc8528d2586db6034bb7a87e2ccbeb139
crispindeity <shoy1415@gmail.com>
2024-01-11 12:53:32 +0900

    :white_check_mark: Add: template method pattern
      - 템플릿 메서드 패턴 학습 테스트 추가

---
+++ 신희철/spring-core-advanced/src/test/java/com/crispin/springcoreadvanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,63 @@
+package com.crispin.springcoreadvanced.trace.template;
+
+import com.crispin.springcoreadvanced.trace.template.code.AbstractTemplate;
+import com.crispin.springcoreadvanced.trace.template.code.SubClassLogic1;
+import com.crispin.springcoreadvanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        log.info("비즈니스 로직1 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        log.info("비즈니스 로직2 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        template2.execute();
+    }
+}
