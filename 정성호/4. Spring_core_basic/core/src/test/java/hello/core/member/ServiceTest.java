e9ff4e169fff306d581c4ca80cb2f764508273da
Your Name <kjeng7897@gmail.com>
2024-01-24 05:09:12 +0900

    add

--- 정성호/4. Spring_core_basic/core/src/test/java/hello/core/member/ServiceTest.java
+++ 정성호/4. Spring_core_basic/core/src/test/java/hello/core/member/ServiceTest.java
@@ -1,4 +1,13 @@
 package hello.core.member;
 
+import org.junit.jupiter.api.Test;
+
 public class ServiceTest {
+    @Test
+    void join() {
+        //given
+        Member meber = new Member(1L, "memberA", Grade.VIP);
+        //when
+
+        //then
 }
