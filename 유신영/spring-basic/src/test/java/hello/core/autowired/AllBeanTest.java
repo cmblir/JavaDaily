66c8aad0f02798b6fa795f34d9744e6752f5f6ed
cmblir <sodlalwl13@gmail.com>
2024-02-11 21:29:57 +0900

    add .java: inflean spring basic spring empty life cycle chapter8

--- 유신영/spring-basic/src/test/java/hello/core/autowired/AllBeanTest.java
+++ 유신영/spring-basic/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -36,7 +36,6 @@ public class AllBeanTest {
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policies;
 
-        @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
             this.policyMap = policyMap;
             this.policies = policies;
