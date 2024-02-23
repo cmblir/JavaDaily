ae292da563527b4f5d8caa4c49e944eaf0df69e0
cmblir <sodlalwl13@gmail.com>
2024-02-23 23:49:35 +0900

    fix .java : baekjoon

--- 유신영/BaekjoonTest/src/Main.java
+++ 유신영/BaekjoonTest/src/Main.java
@@ -1,38 +1,41 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] num = br.readLine().split(" ");
-        int N = Integer.parseInt(num[0]);
-        int M = Integer.parseInt(num[1]);
-        int[] answer = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            answer[i] = i + 1;
-        }
+        int king = 1;
+        int queen = 1;
+        int rook = 2;
+        int bishop = 2;
+        int knight = 2;
+        int pawn = 8;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        king = king - Integer.parseInt(st.nextToken());
+        queen = queen - Integer.parseInt(st.nextToken());
+        rook = rook - Integer.parseInt(st.nextToken());
+        bishop = bishop - Integer.parseInt(st.nextToken());
+        knight = knight - Integer.parseInt(st.nextToken());
+        pawn = pawn - Integer.parseInt(st.nextToken());
 
-        for (int m = 0; m < M; m++) {
-            String[] temp = br.readLine().split(" ");
-            int i = Integer.parseInt(temp[0]);
-            int j = Integer.parseInt(temp[1]);
-            if (i < j) {
-                for (int jm = j-1, im = i-1; jm > im; jm--, im++) {
-                    int tempInt = answer[jm];
-                    answer[jm] = answer[im];
-                    answer[im] = tempInt;
-                }
-            }
-        }
-        for (int i = 0; i < answer.length; i++) {
-            bw.write(answer[i] + " ");
-        }
+        bw.write(king + " ");
+        bw.write(queen + " ");
+        bw.write(rook + " ");
+        bw.write(bishop + " ");
+        bw.write(knight + " ");
+        bw.write(pawn + " ");
+
         bw.flush();
         bw.close();
+
     }
 }
