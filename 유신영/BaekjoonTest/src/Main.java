501ebe6143c6b9030383cb60f62d38f9c0a1bd48
cmblir <sodlalwl13@gmail.com>
2024-03-19 21:20:30 +0900

    add .java: inflean spring basic empty scope chapter9

--- 유신영/BaekjoonTest/src/Main.java
+++ 유신영/BaekjoonTest/src/Main.java
@@ -1,18 +1,20 @@
+import java.io.IOException;
 import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int K = Integer.parseInt(br.readLine());
+        long N = Long.parseLong(br.readLine());
+        br.close();
 
-        bw.write(String.valueOf(K*N));
+        bw.write(String.valueOf(N*N*N));
+        bw.write("\n");
+        bw.write(String.valueOf(3));
         bw.flush();
         bw.close();
     }
---
+++ 유신영/spring-basic/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+    @Test
+    public void PrototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
