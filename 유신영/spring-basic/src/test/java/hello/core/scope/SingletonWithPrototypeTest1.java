479cd31c8b2dd0b4cc285c2054bd6b64348d3a8f
cmblir <sodlalwl13@gmail.com>
2024-04-26 01:33:16 +0900

    add .java: inflean spring scope - singletonBean problem chapter10

--- 유신영/spring-basic/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ 유신영/spring-basic/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,8 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,37 @@ public class SingletonWithPrototypeTest1 {
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
 
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+
+    @Scope("singleton")
+    static class ClientBean {
+
+        @Autowired
+        private ApplicationContext ac;
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+
+        public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
