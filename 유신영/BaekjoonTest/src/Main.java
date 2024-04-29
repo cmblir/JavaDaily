427583ed20130ba17262a7fa856f1aa0ff0a06c8
cmblir <sodlalwl13@gmail.com>
2024-04-29 23:55:25 +0900

    fix .java : LRU Algorithm

--- 유신영/BaekjoonTest/src/Main.java
+++ 유신영/BaekjoonTest/src/Main.java
@@ -1,25 +1,27 @@
+import java.util.LinkedList;import java.util.List;
 public class Main {
-
-    public String solution(String s, int n) {
-        String answer = "";
-        for(int i=0; i<s.length(); i++) {
-            char ch = s.charAt(i);
-            if(ch==' ') { //공백
-                answer += ch;
-                continue;
-            }
-            if(ch>='a' && ch<='z') { //소문자
-                if(ch+n > 'z') {
-                    answer += (char)(ch-26+n);
-                }else {
-                    answer += (char)(ch+n);
-                }
-            }else if(ch>='A' && ch<='Z') { //대문자
-                if(ch+n > 'Z') {
-                    answer += (char)(ch-26+n);
-                }else {
-                    answer += (char)(ch+n);
-                }
+    public static void main(String[] args) {
+        System.out.println(solution(3, new String[] {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"}));
+        System.out.println(solution(3, new String[] {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"}));
+    }
+    public static int solution(int cacheSize, String[] cities) {
+        int answer = 0;         // 캐시크기가 0일 경우
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+        List<String> caches = new LinkedList<>();
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i].toLowerCase(); // 대소문자 구분이 없다.            // cache miss
+            if (!caches.contains(city)) {
+                answer += 5;
+                if (caches.size() >= cacheSize) {                    // 가장 앞에 있는것이 오랫동안 사용하지 않은 캐시므로 0번째를 제거
+                    caches.remove(0);                }
+                caches.add(city);
+                continue;            }             // cache hit
+            if (caches.contains(city)) {
+                caches.remove(city);
+                caches.add(city);
+                answer += 1;
             }
         }
         return answer;
