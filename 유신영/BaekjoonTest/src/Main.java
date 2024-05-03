76c996c1216feb2f49ad5957eabd0426dab0b491
cmblir <sodlalwl13@gmail.com>
2024-05-03 22:38:07 +0900

    fix .java : static

--- 유신영/BaekjoonTest/src/Main.java
+++ 유신영/BaekjoonTest/src/Main.java
@@ -1,38 +1,25 @@
-import java.util.ArrayList;
-
-private static void Main(String[] people ) {
-    int r = 3;
-    boolean[] isChecked = new boolean[people.length];
-    String[] result = new String[r];
-    ArrayList<String[]> totalList = new ArrayList<String[]>();
-
-    permutation(people, isChecked, result, r, 0, totalList);
-
-    for (String[] strings : totalList) {
-        String temp = "";
-        for( String text : strings ) {
-            temp += " " + text;
+class Connection {
+    private static Connection _inst = null;
+    private int count = 0;
+    static public Connection get() {
+        if(_inst == null) {
+            _inst = new Connection();
+            return _inst;
         }
-        System.out.println(temp);
+        return _inst;
     }
-    System.out.println("총 경우의 수 : " + totalList.size());
+    public void count() { count++; };
+    public int getCount() { return count; }
 }
-
-private static void permutation( String[] people, boolean[] isChecked, String[] result, int endPoint, int dept, ArrayList<String[]> totalList ) {
-    if( endPoint == dept ) {
-        totalList.add(result.clone());
-    } else {
-        for ( int i = 0; i < people.length; i++ ) {
-            if( !isChecked[i] ) {
-                isChecked[i] = true; // 사용된 배열 위치
-                result[dept] = people[i]; // 저장
-                permutation(people, isChecked, result, endPoint, dept + 1, totalList);
-                isChecked[i] = false; // 사용된 것 다시 제자리
-                result[dept] = ""; // 저장된 것 제자리
-            }
-        }
+public class Main {
+    public static void main(String[] args) {
+        Connection conn1 = Connection.get();
+        conn1.count();
+        Connection conn2 = Connection.get();
+        conn2.count();
+        Connection conn3 = Connection.get();
+        conn3.count();
+        conn1.count();
+        System.out.print(conn1.getCount());
     }
-}
-
-public void main() {
 }
