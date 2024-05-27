96e0b4ac120a80050dd3039153e53e892ac644f9
cmblir <sodlalwl13@gmail.com>
2024-05-27 23:55:37 +0900

    add .java: inflearn spring mvc 1 sevlet

---
+++ 유신영/servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,24 @@
+package hello.servlet.basic;
+
+public class HelloData {
+
+    private String username;
+    private int age;
+
+    // ==== lombok 생성 코드 ==== //
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
